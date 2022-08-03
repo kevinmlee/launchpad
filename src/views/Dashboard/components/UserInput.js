c2a05ce8a4f9f83de8801d8056d8610b9e34637d
Kevin Lee <kmlee1127@gmail.com>
2022-08-02 19:53:07 -0700

    work on user input component

--- src/views/Dashboard/components/UserInput.js
+++ src/views/Dashboard/components/UserInput.js
@@ -1,13 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { CssVarsProvider } from "@mui/joy/styles";
-import { Button, Typography } from "@mui/joy";
+import { Button, Typography, TextField } from "@mui/joy";
+
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
+
+import "./UserInput.css";
 
 export default function UserInput() {
   useEffect(() => {}, []);
 
   return (
     <CssVarsProvider>
-      <Typography level="h1">UserInput</Typography>
+      <div className="user-input">
+        <TextField
+          classes={{ root: "searchField" }}
+          id="search"
+          label="Plain"
+          placeholder="Search by name or event"
+          variant="plain"
+        />
+
+        <Select id="date" defaultValue="">
+          <Option value="">Date</Option>
+          <Option value="dog">Dog</Option>
+          <Option value="cat">Cat</Option>
+        </Select>
+
+        <Select id="location" defaultValue="">
+          <Option value="">Location</Option>
+          <Option value="dog">Dog</Option>
+          <Option value="cat">Cat</Option>
+        </Select>
+
+        <Select id="event" defaultValue="">
+          <Option value="">Event</Option>
+          <Option value="launch">Launch</Option>
+        </Select>
+
+        <Button
+          id="search-btn"
+          color="primary"
+          onClick={(e) => {}}
+          size="md"
+          variant="solid"
+        >
+          Search
+        </Button>
+      </div>
     </CssVarsProvider>
   );
 }
