c20604f85bc8a9eb01430138b66ea0afd6d21757
Kevin Lee <kmlee1127@gmail.com>
2022-08-03 17:12:01 -0700

    add cards

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -5,21 +5,24 @@ import { Button, Typography } from "@mui/joy";
 import Hero from "./components/Hero";
 import Cards from "./components/Cards";
 import sampleData from "./sampleData.json";
+import upcoming from "./upcomingLaunches.json";
 
 export default function Dashboard() {
   const [launches, setLaunches] = useState({});
 
   useEffect(() => {
     //getLaunches();
-    setLaunches(sampleData);
+    setLaunches(upcoming);
   }, []);
 
   const getLaunches = async () => {
     // https://ll.thespacedevs.com/2.2.0/launch/?limit=10&offset=10
-    await fetch("https://ll.thespacedevs.com/2.2.0/launch/?limit=10")
+    await fetch("https://ll.thespacedevs.com/2.2.0/launch/upcoming?limit=10")
       .then((response) => response.json())
       .then((data) => setLaunches(data));
   };
+
+  //console.log(launches);
 
   return (
     <CssVarsProvider>
--- src/views/Dashboard/components/Cards.js
+++ src/views/Dashboard/components/Cards.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { CssVarsProvider } from "@mui/joy/styles";
-import { Button, TextField } from "@mui/joy";
+import { Button, TextField, Chip } from "@mui/joy";
 import dayjs from "dayjs";
 
 import "./Cards.css";
@@ -8,28 +8,63 @@ import "./Cards.css";
 export default function Cards({ data }) {
   useEffect(() => {}, []);
 
-  console.log(data);
+  const completion = (type) => {
+    let color = "neutral";
+    if (type === "Success" || type === "Go") color = "success";
+    if (type === "Failure") color = "danger";
+    if (type === "TBC" || type === "TBD") color = "warning";
+
+    return (
+      <Chip className="type" color={color} size="sm">
+        {type}
+      </Chip>
+    );
+  };
 
   return (
     <CssVarsProvider>
-      <div className="cards">
-        {data.results.map((post) => (
-          <div className="card" key={post.id}>
-            <div className="featured-image">
-              <img src={post.image} alt={post.mission.name} />
-            </div>
-            <div class="details">
-              <div className="mission">
-                <h3 className="name">{post.mission.name}</h3>
-                <div className="type">{post.mission.type}</div>
-                <div className="desc">{post.mission.description}</div>
-                <div className="target-date">{post.net}</div>
+      <div className="container">
+        <div className="cards">
+          {"results" in data &&
+            data.results.map((post) => (
+              <div className="card" key={post.id}>
+                <div className="chips">
+                  {"mission" in post && post.mission && (
+                    <Chip className="type" color="neutral" size="sm">
+                      {post.mission.type}
+                    </Chip>
+                  )}
+
+                  {completion(post.status.abbrev)}
+                </div>
+
+                <div className={"featured-image " + (!post.image && "no-img")}>
+                  {post.image && <img src={post.image} loading="lazy" />}
+                </div>
+
+                <div className="details">
+                  {"mission" in post && post.mission ? (
+                    <div className="mission">
+                      <h3 className="name">{post.mission.name}</h3>
+                      <div className="lp">
+                        LSP: {post.launch_service_provider.name}
+                      </div>
+                      <div className="desc">
+                        {post.mission.description && post.mission.description}
+                      </div>
+                      <div className="target-date">
+                        {dayjs(post.net).format("ddd, MMM D, YYYY h:mm A")}
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="mission">
+                      <h3 className="name">No information</h3>
+                    </div>
+                  )}
+                </div>
               </div>
-              <div className="lp">{post.launch_service_provider.name}</div>
-              <div className="status">{post.status.abbrev}</div>
-            </div>
-          </div>
-        ))}
+            ))}
+        </div>
       </div>
     </CssVarsProvider>
   );
