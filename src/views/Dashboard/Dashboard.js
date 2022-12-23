a31a3a3631c8c580abd94cf3079841971f7916fc
Kevin Lee <kmlee1127@gmail.com>
2022-12-23 14:50:47 -0800

    cache launch data

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { CssVarsProvider } from "@mui/joy/styles";
+import dayjs from "dayjs";
 
 import Hero from "../../components/Hero/Hero";
 import Cards from "../../components/Cards/Cards";
 import SolarSystemLoader from "../../components/SolarSystemLoader/SolarSystemLoader";
 
-import upcomingLaunches from "./upcomingLaunches.json";
-import upcomingExpeditions from "./upcomingExpeditions.json";
+//import upcomingLaunches from "./upcomingLaunches.json";
+//import upcomingExpeditions from "./upcomingExpeditions.json";
+
+const endpoint = "https://ll.thespacedevs.com/2.2.0";
+const currentTime = dayjs().format();
 
 // for pagination
 // https://ll.thespacedevs.com/2.2.0/launch/?limit=10&offset=10
@@ -21,27 +25,58 @@ export default function Dashboard() {
     getExpeditions();
 
     setLoading(true);
-    setLaunches(upcomingLaunches);
-    setExpeditions(upcomingExpeditions);
+    //setLaunches(upcomingLaunches);
+    //setExpeditions(upcomingExpeditions);
     setLoading(false);
   }, []);
 
+  // check localStorage for launch data
+  // if none found or timer expired, fetch new data
   const getLaunches = async () => {
-    await fetch("https://ll.thespacedevs.com/2.2.0/launch/upcoming?limit=20")
+    const cachedLaunches =
+      localStorage.getItem("launches") &&
+      JSON.parse(localStorage.getItem("launches"));
+
+    if (cachedLaunches) {
+      const difference = dayjs(currentTime).diff(dayjs(cachedLaunches.at));
+      const minutes = Math.floor((difference / 1000 / 60) % 60);
+
+      if (minutes < 30) setLaunches(cachedLaunches);
+    }
+    await fetch(`${endpoint}/launch/upcoming?limit=20`)
       .then((response) => response.json())
-      .then((data) => setLaunches(data));
+      .then((data) => {
+        const cache = data;
+        cache["at"] = currentTime;
+
+        localStorage.setItem("launches", JSON.stringify(cache));
+        setLaunches(cache);
+      });
   };
 
+  // check localStorage for expedition data
+  // if none found or timer expired, fetch new data
   const getExpeditions = async () => {
-    await fetch(
-      "https://ll.thespacedevs.com/2.2.0/expedition?ordering=-start&limit=20"
-    )
-      .then((response) => response.json())
-      .then((data) => setExpeditions(data));
-  };
+    const cachedExpeditions =
+      localStorage.getItem("expeditions") &&
+      JSON.parse(localStorage.getItem("expeditions"));
 
-  //console.log(launches);
-  //console.log(expeditions);
+    if (cachedExpeditions) {
+      const difference = dayjs(currentTime).diff(dayjs(cachedExpeditions.at));
+      const minutes = Math.floor((difference / 1000 / 60) % 60);
+
+      if (minutes < 30) setExpeditions(cachedExpeditions);
+    }
+    await fetch(`${endpoint}/expedition?ordering=-start&limit=20`)
+      .then((response) => response.json())
+      .then((data) => {
+        const cache = data;
+        cache["at"] = currentTime;
+
+        localStorage.setItem("expeditions", JSON.stringify(cache));
+        setExpeditions(cache);
+      });
+  };
 
   return (
     <CssVarsProvider>
