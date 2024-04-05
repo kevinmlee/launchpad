0410f0d8e8257d2838146d86444275a32a152d01
Kevin Lee <kmlee1127@gmail.com>
2024-04-05 20:24:49 +0000

    add Cards

--- src/components/Hero/Hero.spec.js
+++ src/components/Hero/Hero.spec.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 
 import Hero from './Hero'
 
 describe('Hero', () => {
-  it('renders Launchpad title', () => {
-    render(<Hero />);
+  it('renders title', () => {
+    render(<Hero />)
     expect(screen.getByText('Launchpad')).toBeInTheDocument()
   })
 
   it('renders hero copy', () => {
     render(<Hero />)
     expect(screen.getByText('Upcoming launches, dockings, and expeditions')).toBeInTheDocument()
-  })
-
-  it('renders Moon component', () => {
-    render(<Hero />)
-    expect(screen.getByTestId('moon-component')).toBeInTheDocument()
   })
 })
