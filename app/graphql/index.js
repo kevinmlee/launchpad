beae5aee7365a22c35e374e62a8459a1a3098aad
Kevin Lee <kmlee1127@gmail.com>
2026-01-19 13:10:28 -0800

    restore singleton

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -2,28 +2,18 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const GRAPHQL_ENDPOINT = "https://graphql.kevinmlee.com/";
 
-function createApolloClient() {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: GRAPHQL_ENDPOINT,
-      fetch,
-    }),
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      query: {
-        fetchPolicy: "no-cache",
-        errorPolicy: "all",
-      },
+const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: GRAPHQL_ENDPOINT,
+    fetch,
+  }),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
     },
-  });
-}
-
-// Create a fresh client for each server request
-const apolloClient = {
-  query: async (options) => {
-    const client = createApolloClient();
-    return client.query(options);
   },
-};
+});
 
 export default apolloClient;
