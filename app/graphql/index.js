8bd259f17c7e2eba31cd45b9f4004bd42efb9bb4
Kevin Lee <kmlee1127@gmail.com>
2026-01-19 20:09:57 -0800

    remove debug

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -4,16 +4,13 @@ import { registerUrql } from '@urql/next/rsc'
 const isClient = typeof window !== 'undefined'
 
 const makeClient = () => {
-  const apiKey = process.env.GRAPHQL_API_KEY || ''
-  console.log('Creating GraphQL client - API Key present:', !!apiKey, 'Length:', apiKey.length)
-
   return createClient({
     url: 'https://graphql.kevinmlee.com/',
     exchanges: [cacheExchange, fetchExchange],
     requestPolicy: isClient ? 'cache-first' : 'network-only',
     fetchOptions: {
       headers: {
-        'x-api-key': apiKey,
+        'x-api-key': process.env.GRAPHQL_API_KEY || '',
       },
     },
   })
