657671b174777ecb18a5ca0e5342af93cb637d31
Kevin Lee <kmlee1127@gmail.com>
2026-01-20 06:06:28 -0800

    feat: use cache (#192)
    * use cache
    
    * use skeleton

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
+import { Suspense } from "react"
 import Hero from "../src/components/Hero/Hero"
 import Cards from "../src/components/Cards/Cards"
+import { CardSkeletonList } from "../src/components/Cards/CardSkeleton"
 import { GET_ALL_SPACE_DATA } from "./graphql/queries/space"
 import { getClient } from "./graphql"
 
-export const dynamic = "force-dynamic"
+// Revalidate every 5 minutes (300 seconds)
+export const revalidate = 300
 
 const getSpaceData = async () => {
   return getClient()
     .query(GET_ALL_SPACE_DATA)
     .then(result => {
-      console.log('graphql response', result)
       return result?.error ? result.error : result?.data?.space
     })
     .catch(() => ({ error: 'Error fetching data' }))
@@ -21,7 +23,9 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <Cards launches={launches} expeditions={expeditions} events={events} />
+      <Suspense fallback={<CardSkeletonList count={5} />}>
+        <Cards launches={launches} expeditions={expeditions} events={events} />
+      </Suspense>
     </>
   )
 }
